9ca353c647a435de42f6dd31a23b057a8ae44ee9
Daidemang Francis <dfranc15@my.chemeketa.edu>
2018-10-07 19:17:30 -0700

    Create main.cpp

---
+++ Rock Paper Scissors/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int player1, player2;
+	int rock = 1, paper = 2, scissors = 3;
+	cin >> player1 >> player2;
+	
+	if (player1 == player2) {
+		cout << "tie";
+	}
+	else if (player1 == rock && player2 == scissors) {
+		cout << "A";
+	}
+	else if (player1 == paper && player2 == rock) {
+		cout << "A";
+	}
+	else {
+		cout << "B";
+	}
+	
+	
+}
