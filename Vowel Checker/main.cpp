66493fe175da8c2aaf42357595234be6cb846e5d
Daidemang Francis <dfranc15@my.chemeketa.edu>
2018-10-09 17:57:10 -0700

    Update main.cpp

--- Vowel Checker/main.cpp
+++ Vowel Checker/main.cpp
@@ -1,18 +1,17 @@
 #include <iostream>
-#include <string>
+#include <cctype>
 using namespace std;
 
 int main()
 {
-    string vowel = "aeiouAEIOU";
-	char userInput;
-	cin >> userInput;
-	for(int i = 0; i < vowel.length(); i++) {
-		if (userInput == vowel.at(i)) {
-			cout << "vowel";
-		}
-		else {
-			
-		}
+ 	char character;
+	cin >> character;
+	char newCharacter = tolower(character);
+	if(newCharacter == 'a' || newCharacter == 'e' || newCharacter == 'i' || newCharacter == 'o' || newCharacter == 'u') {
+		cout << "vowel";
 	}
+	else {
+		cout << "consonant";
+	}
+
 }
