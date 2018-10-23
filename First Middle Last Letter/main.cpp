81c4dde6fb992af61a3eee1bc05a4cb946aea338
Daidemang Francis <dfranc15@my.chemeketa.edu>
2018-10-22 18:24:10 -0700

    Update main.cpp

--- First Middle Last Letter/main.cpp
+++ First Middle Last Letter/main.cpp
@@ -4,6 +4,15 @@ using namespace std;
 int main()
 {
     string word, firstCharacter, lastcharacter, middleCharacter;
+	int length;
 	cin >> word;
+	
+	length = word.length();
+	if(length % 2 == 0) {
+		//middleCharacter = 
+	}
+	firstCharacter = word.at(0);
+	lastcharacter = word.back();
+	cout << firstCharacter << lastcharacter;
 	
 }
