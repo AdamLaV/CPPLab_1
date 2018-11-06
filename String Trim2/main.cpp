f066ff8d9010acea66dc70af97329d537fe255e4
Daidemang Francis <dfranc15@my.chemeketa.edu>
2018-11-06 15:53:46 -0800

    Update main.cpp

--- String Trim2/main.cpp
+++ String Trim2/main.cpp
@@ -1,1 +1,23 @@
-cc
+#include <iostream>
+#include <string>
+using namespace std;
+//Do not modify anything on or above the line below this
+//YOUR_CODE_BELOW
+
+string stringTrim(string word) {
+	size_t index = word.length();
+	word = word.substr(1, index - 2);
+	return word;
+}
+
+
+//YOUR_CODE_ABOVE
+//Do not modify anything on or below the line above this
+int main()
+{
+    string word;
+    cin >> word;
+    string result = stringTrim(word);
+    cout << result << endl;
+
+}
