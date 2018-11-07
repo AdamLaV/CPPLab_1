e5b53b099164626d374a5a02f5f443452b4a1e71
Daidemang Francis <dfranc15@my.chemeketa.edu>
2018-11-06 19:47:55 -0800

    Create main.cpp

---
+++ Printf Fraction/main.cpp
@@ -0,0 +1,19 @@
+#include <cstdio>
+#include <cstring>
+
+int main()
+{
+    int numerator, denominator;
+    scanf("%d %d", &numerator, &denominator);
+//Do not modify anything on or above the line below this
+//YOUR_CODE_BELOW
+
+    double total = 1.0 * numerator / denominator;
+	//printf("%5d",);
+	printf("%d/%d = %.3lf", numerator, denominator, total);
+
+
+//YOUR_CODE_ABOVE
+//Do not modify anything on or below the line above this
+
+}
