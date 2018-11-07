4efda3ba08bff869eb7fb01173f374f926906613
Daidemang Francis <dfranc15@my.chemeketa.edu>
2018-11-06 19:07:34 -0800

    Create main.cpp

---
+++ C Money Printer/main.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+#include <cstring>
+
+int main()
+{
+//Do not modify anything on or above the line below this
+//YOUR_CODE_BELOW
+
+    double amount; 
+	char sign = '$';
+	scanf("%lf", &amount);
+	double rounded = ((int)(amount * 100 + .5) / 100.0);
+	printf("%c""%.2lf", sign, rounded);
+
+//YOUR_CODE_ABOVE
+//Do not modify anything on or below the line above this
+
+}
