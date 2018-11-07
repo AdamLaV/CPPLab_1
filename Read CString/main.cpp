8a81f8c70478dd76e7f563166db629ef806acf02
Daidemang Francis <dfranc15@my.chemeketa.edu>
2018-11-07 07:50:10 -0800

    Update and rename C to F/main.cpp to Read CString/main.cpp

---
+++ Read CString/main.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+#include <cstring>
+
+int main()
+{
+//Do not modify anything on or above the line below this
+//YOUR_CODE_BELOW
+
+    char string[20];
+	scanf("%s", string);
+	printf("%s\n", string);
+
+//YOUR_CODE_ABOVE
+//Do not modify anything on or below the line above this
+
+}
