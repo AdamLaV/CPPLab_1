a8c06a69e907ecb4d066e83570797906ffc9994e
Daidemang Francis <dfranc15@my.chemeketa.edu>
2018-11-06 18:48:44 -0800

    Create main.cpp

---
+++ Simple Scanf/main.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+#include <cstring>
+
+int main()
+{
+//Do not modify anything on or above the line below this
+//YOUR_CODE_BELOW
+
+    int num1, num2, num3, total = 0;
+	scanf("%d %d %d", &num1, &num2, &num3);
+	total += num1 + num2 + num3;
+	printf("%d", total);
+
+//YOUR_CODE_ABOVE
+//Do not modify anything on or below the line above this
+
+}
