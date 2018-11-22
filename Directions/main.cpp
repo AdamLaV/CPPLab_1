7a3ff025dc175fe9c162fa7fb6b8ed4f7c718bb9
Daidemang Francis <dfranc15@my.chemeketa.edu>
2018-11-22 07:06:10 -0800

    Create main.cpp

---
+++ Directions/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+//Do not modify anything on or above the line below this
+//YOUR_CODE_BELOW
+
+enum Direction {
+	NORTH = 90,
+	SOUTH = 270,
+	EAST = 0,
+	WEST = 180,
+};
+
+//YOUR_CODE_ABOVE
+//Do not modify anything on or below the line above this
+int main()
+{
+
+    int d1;
+    cin >> d1;
+
+    //map values over 360 and greater back into 0-359 range
+    d1 %= 360;
+
+    if(d1 == NORTH) {
+        cout << "North" << endl;
+    }
+    else if(d1 == SOUTH) {
+        cout << "South" << endl;
+    }
+    else if(d1 == EAST) {
+        cout << "East" << endl;
+    }
+    else if(d1 == WEST) {
+        cout << "West" << endl;
+    }
+    else {
+        cout << "Error" << endl;
+    }
+
+}
