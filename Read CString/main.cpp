680721295658d9aa98278648a7260cf4d8472ed3
Daidemang Francis <dfranc15@my.chemeketa.edu>
2019-03-17 09:54:04 -0700

    Update main.cpp

--- Read CString/main.cpp
+++ Read CString/main.cpp
@@ -3,14 +3,7 @@
 
 int main()
 {
-//Do not modify anything on or above the line below this
-//YOUR_CODE_BELOW
-
     char string[20];
 	scanf("%s", string);
 	printf("%s\n", string);
-
-//YOUR_CODE_ABOVE
-//Do not modify anything on or below the line above this
-
 }
